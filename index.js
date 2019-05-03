8e3f905915be151ada462161ec07909bfd309090
ritvik <ritvik.mandyam@gmail.com>
2019-05-03 14:54:48 -0700

    Created login.html

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', (req, res) =>  res.sendFile(__dirname + '/public/index.html'));
+app.get('/', (req, res) =>  res.sendFile(__dirname + '/public/login.html'));
+app.get('/playlist', (req, res) =>  res.sendFile(__dirname + '/public/index.html'));
 
 app.listen(port, '0.0.0.0');
